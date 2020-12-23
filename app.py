f31731a7f42d929bf54f00044bf14321e946124d
Markus <mail@huber-markus.com>
2020-12-23 15:24:40 +0100

    adding a form to generate the link

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 from datetime import datetime
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask import render_template
 import math
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/", methods=["POST", "GET"])
+def editor():
+    if request.method == "POST":
+        url = request.url
+        sender = request.form["sender"]
+        receiver = request.form["receiver"]
+        date = request.form["date"]
+        if not sender or not receiver or not date:
+            return render_template('editor.html',link = None, error = True)
+        try:
+            datetime.strptime(date, '%Y-%m-%d-%H-%M')
+        except:
+            return render_template('editor.html',link = None, error = True)
+
+        link = url + "r?sender=" + sender + "&receiver=" + receiver + "&date=" + date
+        return render_template('editor.html',link = link)
+    else:
+        return render_template('editor.html',link = None)
+
+@app.route("/r")
+def reminder():
+    print("responding")
     sender = request.args.get('sender')
     receiver = request.args.get('receiver')
     send_date = request.args.get('date')
     if not sender or not receiver or not send_date:
-        url = request.url_root
-        return render_template('tutorial.html',url=url)
+        return redirect(url_for("editor"))
     try:
         time_send = datetime.strptime(send_date, '%Y-%m-%d-%H-%M')
     except:
-        url = request.url_root
-        return render_template('tutorial.html', url=url)
+        return redirect(url_for("editor"))
     duration = datetime.now()-time_send
     days = duration.days
     hours = math.floor(duration.seconds/3600)
